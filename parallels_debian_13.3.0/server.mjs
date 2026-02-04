9f4375cf1bbda74c832fa0fa90e2c4e731efac6a
tushar8133 <tushar8133@gmail.com>
2026-02-04 16:50:01 +0530

    debian node server updates

--- parallels_debian_13.3.0/server.mjs
+++ parallels_debian_13.3.0/server.mjs
@@ -1,16 +1,18 @@
 import express from "express";
+import os from "os";
 
 const app = express();
 
 app.use((req, res, next) => {
-  console.log(Date.now())
+  console.log(`File Accessed at ${Date.now()}`);
   next();
 })
+
 app.use(express.static("public"));
+
 app.use("/", express.static("."));
 
 app.listen(3000, () => {
-  console.log("SERVER STARTED");
+  const ip = os.networkInterfaces()?.en0?.filter(o => o?.family === "IPv4")?.[0]?.address;
+  console.log(`http://${ip}:3000/preseed.cfg?nocache=1`);
 });
-
-
